d6e2a64cd0d6cd76e48f5f151d899c44ca589cc0
Rob Jones <rjones@cscos.com>
2018-04-10 20:40:55 -0400

    working on elevator movement

--- models.js
+++ models.js
@@ -4,15 +4,16 @@ function simulation(num_elevators, num_floors) {
 
     this.elevator_controller = new elevator_controller(this.num_elevators, this.num_floors);
 
-    this.request_elevator = function (floor) {
-
+    this.request_elevator = function (on_floor, to_floor) {
+        this.elevator_controller.dispatch_elevator(this.elevator_controller.find_best_elevator(on_floor), to_floor)
     }
 }
 
-function elevator(id) {
+function elevator(id, top_floor) {
     this.id = id;
     this.current_floor = 1;
-    this.desired_floor = 1;
+    this.desired_floors = [];
+    this.top_floor = top_floor;
     this.door_closed = true;
     this.going_up = true;
     this.occupied = false;
@@ -22,29 +23,50 @@ function elevator(id) {
     this.trips_until_maintenance = 100;
 
     this.start_trip = function (desired_floor) {
-        this.desired_floor = desired_floor;
+        this.desired_floors.push(to_floor);
         // Report when doors close
     }
 
     this.next_floor = function () {
         // Confirm we are not on the top floor or bottom floor
-        // Report when moving from floor to floor
-        this.num_floors += 1;
+        if (this.going_up && current_floor === top_floor) {
+            this.going_up = false;
+            current_floor -= 1;
+        } else if (!this.going_up && current_floor === 1) {
+            this.going_up = true;
+            current_floor += 1;
+        } else {
+            this.going_up ? current_floor += 1 : current_floor -= 1;
+        }
+
+        if (current_floor === desired_floor)
+
+            // Report when moving from floor to floor
+            this.num_floors += 1;
+
+
 
     }
 
     this.end_trip = function () {
+        this.stopped = true;
+        this.door_closed = false;
+
         // Report when doors open
         this.number_of_trips += 1;
     }
 }
 
 function elevator_controller(num_elevators, num_floors) {
-    // TODO: init elevators
     this.elevators = [];
+    var id = 1
+    while (this.elevators.length < num_elevators) {
+        this.elevators.push(new elevator(id, num_floors));
+        id++;
+    }
 
     this.find_best_elevator = function (desired_floor) {
-        var possible_elevators = this.elevators.filter(e => (e.current_floor > desired_floor && !e.going_up && !e.stopped)
+        var possible_elevators = this.elevators.filter(e => e.trips_until_maintenance !== 0).filter(e => (e.current_floor > desired_floor && !e.going_up && !e.stopped)
             || (e.current_floor < desired_floor && e.going_up && !e.stopped) || !e.occupied);
         var best_elevator;
         var lowest_distance = num_floors;
@@ -56,19 +78,20 @@ function elevator_controller(num_elevators, num_floors) {
                 best_elevator = elevator;
             }
         }
+        return best_elevator;
     }
 
     // this returns an integer based on how likely this elevator 
     // is going to be the best fit to answer the request
     this.calculate_distance = function (requested_floor, elevator) {
-        // if the elevator is occupied add one distance
-        var occupied_buffer = elevator.occupied ? 1 : 0;
+        // if the elevator is occupied and moving add one floor buffer
+        var occupied_buffer = elevator.occupied && !elevator.stopped ? 1 : 0;
 
         return requested_floor >= elevator.current_floor ? requested_floor + occupied_buffer - elevator.current_floor :
             elevator.current_floor - requested_floor + occupied_buffer;
     }
 
-    this.dispatch_elevator = function (elevator, floor) {
-
+    this.dispatch_elevator = function (elevator, to_floor) {
+        elevator.start_trip(to_floor);
     }
 }
