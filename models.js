e509cd1849696398d9b3bf1d771286a144feae24
Rob Jones <rjones@cscos.com>
2018-04-10 20:24:46 -0400

    improving find best elevator

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ function simulation(num_elevators, num_floors) {
     this.num_elevators = (num_elevators || num_floors < 1) || 1;
     this.num_floors = (num_floors || num_floors < 1) || 1;
 
-    this.elevator_controller = new elevator_controller(this.num_elevators);
+    this.elevator_controller = new elevator_controller(this.num_elevators, this.num_floors);
 
     this.request_elevator = function (floor) {
 
@@ -37,33 +37,37 @@ function elevator(id) {
         // Report when doors open
         this.number_of_trips += 1;
     }
-
-    // this returns an integer based on how likely this elevator 
-    // is going to be the best fit to answer the request
-    this.calculate_best_fit = function (requested_floor) {
-        if (!this.occupied) {
-
-        }
-    }
 }
 
-function elevator_controller(num_elevators) {
+function elevator_controller(num_elevators, num_floors) {
     // TODO: init elevators
     this.elevators = [];
 
     this.find_best_elevator = function (desired_floor) {
-        // check unoccupied elevators
-        var best_elevators = this.elevators.filter(e => !e.occupied);
+        var possible_elevators = this.elevators.filter(e => (e.current_floor > desired_floor && !e.going_up && !e.stopped)
+            || (e.current_floor < desired_floor && e.going_up && !e.stopped) || !e.occupied);
         var best_elevator;
-        if (best_elevators.length < 1) {
-            // check occupied elevators moving towards the desired floor
-            best_elevators = this.elevators.filter(e => (e.current_floor > desired_floor && !e.going_up && !e.stopped)
-                || (e.current_floor < desired_floor && e.going_up && !e.stopped));
-                            
-        } else {
-             best_elevator = this.best_elevators.filter(e => e.current_floor === desired_floor)
+        var lowest_distance = num_floors;
+        for (var index = 0; index < this.possible_elevators.length; index++) {
+            var elevator = possible_elevators[index];
+            var distance_from_floor = this.calculate_distance(desired_floor, elevator);
+            if (distance_from_floor < lowest_distance) {
+                lowest_distance = distance_from_floor;
+                best_elevator = elevator;
+            }
         }
     }
+
+    // this returns an integer based on how likely this elevator 
+    // is going to be the best fit to answer the request
+    this.calculate_distance = function (requested_floor, elevator) {
+        // if the elevator is occupied add one distance
+        var occupied_buffer = elevator.occupied ? 1 : 0;
+
+        return requested_floor >= elevator.current_floor ? requested_floor + occupied_buffer - elevator.current_floor :
+            elevator.current_floor - requested_floor + occupied_buffer;
+    }
+
     this.dispatch_elevator = function (elevator, floor) {
 
     }
